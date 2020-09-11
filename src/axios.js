36640ef46dafdbeb0f90d4c91c334a4dbeb71204
chetan <chetan.oli@squareyards.co.in>
2020-09-11 20:58:33 +0530

    Finally working url

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,8 @@ import axios from 'axios';
  * baseURL here is the API(cloud fuction) url
  */
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-6de06/us-central1/api'
+    // baseURL: 'http://localhost:5001/clone-6de06/us-central1/api'
+    baseURL : 'https://us-central1-clone-6de06.cloudfunctions.net/api'
 })
 
 export default instance;
