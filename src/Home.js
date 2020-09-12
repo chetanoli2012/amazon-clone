2d5fea5f2d340417f0e9df5a05a068093c31ae34
chetan <chetan.oli@squareyards.co.in>
2020-09-12 15:21:35 +0530

    Added slider in homepage

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,57 @@
 import React from 'react';
 import './Home.css';
 import Product from './Product';
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
 function Home() {
+
+    const properties = {
+        duration: 1000,
+        autoplay: true,
+        transitionDuration: 500,
+        arrows: true,
+        infinite: true,
+        // indicators: i => <div className="indicator">{i + 1}</div>
+    };
+
+    const slideImages = [
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Rajeshwari/September/GWBanners/Control/DesktopHero_1500x600._CB405007888_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/SamsungM/MonsterSeries/V203057508_SamsungM_M21_M31_GW_tall_hero_1500x600._CB410822764_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/HFC/LPG/LPG_Hero_PC_1500x600._CB407755280_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img20/Shoes/September/SSW/GW/GW_PC_1500x600._CB404931378_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/IN-hq/2020/img/Sports/XCM_Manual_ORIGIN_1261289_1333735_IN_in_fitness_days_event_sep_gw_in_en_3328889_1500x600_1X_en_IN._CB405086418_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img20/PC/Accessories/GW/PC-acc_june20_DesktopHero_1500x600._CB429195970_.jpg",
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/WLA/September/Headsets/realmeBudsClassic/White/V248982080_WLA-realme_Buds_Classic_White_Mob_Hero_1242x450._CB405393760_.jpg"
+    ];
+
     return (
         <div className='home'>
 
             {/* <p>I am the home component</p> */}
 
             <div className='home__container'>
-                <img className='home__image' src='https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/SamsungM/M51/GWTO/PostLaunch/Uber/P38983965_IN_WLME_SamsungGalaxy_M51_New_Launchtall-hero_1500x600_1._CB405488972_.jpg' alt='Samsung Galazy M51' />
+                {/* <img className='home__image' src='https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/SamsungM/M51/GWTO/PostLaunch/Uber/P38983965_IN_WLME_SamsungGalaxy_M51_New_Launchtall-hero_1500x600_1._CB405488972_.jpg' alt='Samsung Galazy M51' /> */}
+
+                <Slide {...properties}>
+                    {slideImages.map((each, index) => (
+                        //   <div key={index} className="each-slide">
+                        <img key={index} className='home__image' src={each} alt="sample" />
+                        //   </div>
+                    ))}
+                </Slide>
+
+
+                {/* <div className="slide-container">
+                    <Slide duration={2000} {...properties}>
+                        {slideImages.map((each, index) => (
+                            <div key={index} className="each-slide">
+                                <img className="lazy" src={each} alt="sample" />
+                            </div>
+                        ))}
+                    </Slide>
+                </div> */}
 
 
                 <div className='home__row'>
